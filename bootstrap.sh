ce5372f026a354c757362b9ef19a4a31e1306852
Colin Hutchinson <colin@hutchic.com>
2015-08-04 06:03:14 +0000

    Let btsync install all packages but zsh and btsync itself

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,13 +14,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install -yf python curl vim strace diffstat pkg-config cmake build-essential tcpdump mercurial \
-vlc host dnsutils tree dos2unix zip zsh language-pack-en python-pip git zsh wget openjdk-7-jre oracle-java8-installer \
-flashplugin-installer unace unrar synaptic bleachbit openjdk-7-jre oracle-java8-installer unace unrar \
-zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac \
-faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools \
-id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdread4 libdvdnav4 \
-libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* google-chrome-stable
+sudo apt-get install -yf python curl vim zip zsh language-pack-en python-pip git zsh
 
 echo "Cleaning Up"
 sudo apt-get -f install
--- btsync_symlinks.sh
+++ btsync_symlinks.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 sudo usermod -a -G btsync `whoami`
-sudo chmod -R 775 /var/lib/btsync
 mkdir -p /var/lib/btsync/devbox/
 if [ ! -d `pwd`/devbox ]; then
     ln -s /var/lib/btsync/devbox `pwd`/devbox
+fi
+
+if [ ! -d `pwd`/.atom ]; then
+    ln -s `pwd`/devbox/.atom `pwd`/.atom
 fi
 
 if ! [ -L .zsh_history ]; then
@@ -16,3 +19,28 @@ if ! [ -L .ssh ]; then
     rm -rf `pwd`/.ssh
     ln -s `pwd`/devbox/.ssh `pwd`/.ssh
 fi
+
+sudo apt-key add ~/devbox/repo.keys
+sudo cp -R ~/devbox/sources/* /etc/apt/
+sudo apt-get update
+sudo apt-get install dselect
+sudo dselect update
+sudo dpkg --set-selections < ~/devbox/Package.list
+sudo apt-get dselect-upgrade -y
+
+sudo touch /etc/cron.hourly/export_packages
+sudo cat > /etc/cron.hourly/export_packages <<EOF
+#!/bin/bash
+
+sudo dpkg --get-selections > /var/lib/btsync/devbox/Package.list
+sudo mkdir -p /var/lib/btsync/devbox/sources
+sudo cp -R /etc/apt/sources.list* /var/lib/btsync/devbox/sources/
+sudo apt-key exportall > /var/lib/btsync/devbox/repo.keys
+
+sudo chmod -R 775 /var/lib/btsync/devbox/sources
+sudo chmod -R 775 /var/lib/btsync/devbox/repo.keys
+sudo chmod -R 775 /var/lib/btsync/devbox/Package.list
+EOF
+
+sudo chmod +x /etc/cron.hourly/export_packages
+sudo chmod -R 775 /var/lib/btsync
